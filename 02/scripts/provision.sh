0853264da0aea54250bbd7e39a3d574ae44a1c1e
Due Due <orih59@gmail.com>
2016-04-20 14:48:08 +0900

    課題02
    Signed-off-by: Due Due <orih59@gmail.com>

---
+++ 02/scripts/provision.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+
+yum -y update
+yum -y install ntp httpd
+
+sed -i -e "s/Listen 80/Listen 8080/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
+
---
+++ 02/scripts/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eu
+
+function is_running() {
+	SERVICE_NAME=$1
+	IS_SERVICE_RUNNING=$(service $SERVICE_NAME status | grep "running")
+	if [ "$IS_SERVICE_RUNNING" == "" ]; then
+		echo 'false'
+	else
+		echo 'true'
+	fi
+}
+
+IS_NTPD_RUNNING=$(is_running ntpd)
+if [ "$IS_NTPD_RUNNING" == 'true' ]; then
+  service ntpd restart
+else
+  service ntpd start
+fi
+
+IS_HTTPD_RUNNING=$(is_running httpd)
+if [ "$IS_HTTPD_RUNNING" == 'true' ]; then
+  service httpd restart
+else
+  service httpd start
+fi
