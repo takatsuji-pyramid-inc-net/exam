360a5ba6fcb4f686b7892dc950ae42f161942b5b
takatsuji <takatsuji@pyramid-inc.net>
2016-04-21 15:53:24 +0900

    apacheにリダイレクションの設定を加わえた

--- 02/scripts/provision.sh
+++ 02/scripts/provision.sh
@@ -6,4 +6,5 @@ yum -y install ntp httpd
 
 sed -i -e "s/Listen 80/Listen 8080/g" /etc/httpd/conf/httpd.conf
 sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
+echo "Redirect permanent /images/ /assets/" >> /etc/httpd/conf/httpd.conf
 
