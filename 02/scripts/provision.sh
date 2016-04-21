13a7b80b0fa198c2575a32c7fe47cc7f2b9f6a0f
takatsuji <takatsuji@pyramid-inc.net>
2016-04-21 18:42:09 +0900

    プロビジョン時にシンボリックリンクを追加するようにした。

--- 02/scripts/provision.sh
+++ 02/scripts/provision.sh
@@ -7,3 +7,4 @@ yum -y install ntp httpd
 sed -i -e "s/Listen 80/Listen 8080/g" /etc/httpd/conf/httpd.conf
 sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
 
+ln -s /var/www/html/assets/ /var/www/html/images
