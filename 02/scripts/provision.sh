2454c8e6edbaa51359dffb58ea772f0f9b874d5b
takatsuji <takatsuji@pyramid-inc.net>
2016-04-22 19:19:29 +0900

    apacheの設定にurlの書き換えを追加した。

--- 02/scripts/provision.sh
+++ 02/scripts/provision.sh
@@ -6,4 +6,7 @@ yum -y install ntp httpd
 
 sed -i -e "s/Listen 80/Listen 8080/g" /etc/httpd/conf/httpd.conf
 sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
-
+echo "RewriteLog /var/log/httpd/rewrite.log" >> /etc/httpd/conf/httpd.conf
+echo "RewriteLogLevel 9" >> /etc/httpd/conf/httpd.conf
+echo "RewriteEngine on" >> /etc/httpd/conf/httpd.conf
+echo 'RewriteRule /images/(.*)$ /assets/$1' >> /etc/httpd/conf/httpd.conf
