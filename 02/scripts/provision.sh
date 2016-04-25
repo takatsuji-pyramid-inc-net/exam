cef040fecd5add813dccc9fab2c1eef89d2da3d5
takatsuji <takatsuji@pyramid-inc.net>
2016-04-25 12:41:28 +0900

    apacheにAlias設定を追加した。

--- 02/scripts/provision.sh
+++ 02/scripts/provision.sh
@@ -6,4 +6,4 @@ yum -y install ntp httpd
 
 sed -i -e "s/Listen 80/Listen 8080/g" /etc/httpd/conf/httpd.conf
 sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
-
+echo 'Alias /images/ /var/www/html/assets/' >> /etc/httpd/conf/httpd.conf
