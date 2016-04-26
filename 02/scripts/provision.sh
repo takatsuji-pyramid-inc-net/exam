c25d3f54cee3bb621e4a5b0ad43a4b6264d9b828
takatsuji <takatsuji@pyramid-inc.net>
2016-04-26 15:10:23 +0900

    apacheにProxy設定を追加した。

--- 02/scripts/provision.sh
+++ 02/scripts/provision.sh
@@ -7,3 +7,7 @@ yum -y install ntp httpd
 sed -i -e "s/Listen 80/Listen 8080/g" /etc/httpd/conf/httpd.conf
 sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
 
+echo "ProxyPass /images/ http://localhost:8080/assets/" >> /etc/httpd/conf/httpd.conf
+echo "ProxyPassReverse /images/ http://localhost:8080/assets/" >> /etc/httpd/conf/httpd.conf
+echo "ProxyPassReverseCookieDomain localhost:5001 localhost:8080" >> /etc/httpd/conf/httpd.conf
+echo "ProxyPassReverseCookiePath /images/ /assets/" >> /etc/httpd/conf/httpd.conf
