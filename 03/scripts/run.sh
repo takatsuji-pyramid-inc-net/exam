1d737b35fd2d7e3f85d44340c896c73a38c01805
takatsuji <takatsuji@pyramid-inc.net>
2016-05-09 12:00:07 +0900

    basic認証をプロビジョン時に設定出来るようにした

--- 03/scripts/run.sh
+++ 03/scripts/run.sh
@@ -11,6 +11,20 @@ function is_running() {
 	fi
 }
 
+if [ "$1" != '' ]; then
+    sed -i -e "s/AllowOverride None/AllowOverride All/g" /etc/httpd/conf/httpd.conf
+    cat << EOT | tee /var/www/html/.htaccess > /dev/null
+    AuthUserFile /etc/httpd/conf/.htpasswd
+    AuthGroupFile /dev/null
+    AuthName "input your id and passwd"
+    AuthType Basic
+    require valid-user
+EOT
+    htpasswd -c -b /etc/httpd/conf/.htpasswd "$1" "$2"
+else
+    sed -i -e "s/AllowOverride All/AllowOverride None/g" /etc/httpd/conf/httpd.conf
+fi
+
 IS_HTTPD_RUNNING=$(is_running httpd)
 if [ "$IS_HTTPD_RUNNING" == 'true' ]; then
   service httpd restart
