f86961e81b247a04e20d891061cd6129d00c4a9f
takatsuji <takatsuji@pyramid-inc.net>
2016-06-07 21:00:33 +0900

    dbコンテナとphpfpmのコンテナを追加し、wordpressが稼働するようにした。

--- 07/scripts/provision.sh
+++ 07/scripts/provision.sh
@@ -7,11 +7,11 @@ if [ ! -e /usr/bin/docker ]; then
     usermod -aG docker vagrant
 fi
 # docker-composeをインストールする
-if [ ! -e /usr/bin/docker-compose ]; then
-    curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > ~/docker-compose
-    chmod +x ~/docker-compose 
-    mv ~/docker-compose /usr/bin/
-fi
+#if [ ! -e /usr/bin/docker-compose ]; then
+#    curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > ~/docker-compose
+#    chmod +x ~/docker-compose 
+#    mv ~/docker-compose /usr/bin/
+#fi
 
 # wordpressをダウンロードする
 if [ ! -e /usr/local/src/wordpress ]; then
--- 07/scripts/run.sh
+++ 07/scripts/run.sh
@@ -18,5 +18,20 @@ else
     service docker start
 fi
 
-# docker-composeを使えない
+pushd /var/docker > /dev/null
 
+# 各コンテナのビルドを行う
+docker build -t takatsuji/my_db:0.0 ./mysql
+docker build -t takatsuji/my_fpm:0.0 ./phpfpm
+docker build -t takatsuji/my_nginx:0.0 ./nginx
+
+# TODO: --nameオプションは使用しないようにするつもりだったけど、linkどうしよう
+#       同名のコンテナが存在するかをifでチェックするか、対象のイメージを持つ生きているコンテナidを取得するか...
+#       後回し
+
+# 各コンテナの起動を行う
+docker run -d --name my_db takatsuji/my_db:0.0
+docker run -d --name my_fpm --link='my_db:mysql' --volume='/wordpress:/var/www/html' takatsuji/my_fpm:0.0
+docker run -d --name my_nginx -p 80:80 --link='my_fpm:phpfpm' --volume='/wordpress:/var/www/html' takatsuji/my_nginx:0.0
+
+popd > /dev/null
